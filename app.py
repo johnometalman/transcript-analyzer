ab7aaedafc757faec68b8a32efc5483f5d9b3890
johnometalman <johnometalmanld@gmail.com>
2025-03-26 12:09:27 -0500

    v1.3 - Data and Metrics

--- app.py
+++ app.py
@@ -10,6 +10,20 @@ st.set_page_config(
     layout="centered"
 )
 
+# Inject Clarity script
+clarity_script = """
+    <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "qucmxy2erv");
+    </script>
+"""
+
+st.html(clarity_script)  # Inject script
+
+
 # Initialize OpenAI client
 client = OpenAI(
     api_key=st.secrets["DEEPSEEK_API_KEY"],
